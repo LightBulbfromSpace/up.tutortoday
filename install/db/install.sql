c8f5798e31e7876d2e39bdda1ca1bed1493c61a8
Pawel Wolkow <wolkow937@gmail.com>
2023-04-13 22:59:23 +0200

    changed types of columns, made creation of indexes inside creation of table

--- install/db/install.sql
+++ install/db/install.sql
@@ -1,46 +1,42 @@
 CREATE TABLE IF NOT EXISTS up_tutortoday_user (
-    ID bigint auto_increment not null,
-    NAME varchar(63) not null,
-    SURNAME varchar(63) not null,
-    MIDDLE_NAME varchar(63),
+    ID int unsigned auto_increment not null,
+    NAME varchar(100) not null,
+    SURNAME varchar(100) not null,
+    MIDDLE_NAME varchar(100),
     ROLE_ID int not null,
     SUBJECT_ID int,
-    PRIMARY KEY (ID)
+    PRIMARY KEY (ID),
+    INDEX up_tutortoday_tutors_subjects (SUBJECT_ID),
+    INDEX up_tutortoday_users_roles (ROLE_ID)
 );
 
-CREATE INDEX up_tutortoday_tutors_subjects
-    ON up_tutortoday_user (SUBJECT_ID);
-
-CREATE INDEX up_tutortoday_users_roles
-    ON up_tutortoday_user (ROLE_ID);
-
 CREATE TABLE IF NOT EXISTS up_tutortoday_roles (
-    ID int auto_increment not null,
-    NAME varchar(63) not null,
+    ID int unsigned auto_increment not null,
+    NAME varchar(100) not null,
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE IF NOT EXISTS up_tutortoday_subject (
-    ID int auto_increment not null,
-    NAME varchar(63) not null,
+    ID int unsigned auto_increment not null,
+    NAME varchar(100) not null,
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE IF NOT EXISTS up_tutortoday_contacts (
-    ID bigint auto_increment not null,
-    TUTOR_ID bigint not null,
+    ID int unsigned auto_increment not null,
+    TUTOR_ID int unsigned not null,
     PHONE_NUMBER varchar(16),
-    EMAIL varchar(320),
+    EMAIL varchar(255),
     VK_PROFILE varchar(100),
     TELEGRAM_USERNAME varchar(32),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE IF NOT EXISTS up_tutortoday_feedbacks (
-    ID bigint auto_increment not null,
-    TUTOR_ID bigint not null,
+    ID int unsigned auto_increment not null,
+    TUTOR_ID int unsigned not null,
     TITLE varchar(100) not null,
-    DESCRIPTION varchar(500),
+    DESCRIPTION text,
     STARS_NUMBER int,
     PRIMARY KEY (ID)
 );
